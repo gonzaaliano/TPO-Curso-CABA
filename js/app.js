a1d0a2ed8b2cf066d05ac38195d415f654029e30
Gonza Aliano <gonzaaliano@hotmail.com>
2022-05-25 22:05:30 -0300

    Pag Finalizada TPO

--- js/app.js
+++ js/app.js
@@ -5,3 +5,21 @@ $(document).ready(function() {
         $('ul').toggleClass('show');
     });
 });
+
+/*SCROLL NAVBAR*/
+
+// const header = document.querySelector("header");
+// const sectionOne = document.querySelector(background: url(/assets/banner - gimnasio.jpg));
+
+// const sectionOneOptions = {};
+
+// const sectionOneObserver = new IntersectionObserver(function(
+//         entries,
+//         sectionOneObserver) {
+//         entries.forEach(entry => {
+//             console.log(entry.target);
+//         })
+//     },
+//     sectionOneOptions);
+
+// sectionOneObserver.observe(sectionOne);
